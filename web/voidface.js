6ac9476daf4797b7fe06ff60488096dbb6433da1
Stanislav Dymedyuk <stanislav@competition.company>
2025-03-13 20:40:57 +0100

    Added points life cyscle

--- web/voidface.js
+++ web/voidface.js
@@ -2,145 +2,239 @@ window.addEventListener("resize", updateCanvas);
 window.addEventListener("load", initialDraw);
 
 let scene = null;
+const minLifeTicks = 50;
+const maxLifeTicks = 250;
 
 /* Generated static points */
 const maskPoints = [
   { x: 17, y: 6 },
+  { x: 18, y: 6 },
+  { x: 19, y: 6 },
+  { x: 20, y: 6 },
   { x: 21, y: 6 },
+  { x: 22, y: 6 },
   { x: 23, y: 6 },
+  { x: 24, y: 6 },
   { x: 25, y: 6 },
   { x: 50, y: 6 },
+  { x: 51, y: 6 },
   { x: 52, y: 6 },
+  { x: 53, y: 6 },
   { x: 54, y: 6 },
+  { x: 55, y: 6 },
   { x: 56, y: 6 },
+  { x: 57, y: 6 },
   { x: 58, y: 6 },
   { x: 16, y: 7 },
+  { x: 17, y: 7 },
   { x: 18, y: 7 },
+  { x: 19, y: 7 },
   { x: 20, y: 7 },
   { x: 26, y: 7 },
   { x: 27, y: 7 },
   { x: 48, y: 7 },
   { x: 49, y: 7 },
   { x: 55, y: 7 },
+  { x: 56, y: 7 },
   { x: 57, y: 7 },
+  { x: 58, y: 7 },
   { x: 59, y: 7 },
   { x: 15, y: 8 },
+  { x: 16, y: 8 },
   { x: 17, y: 8 },
+  { x: 18, y: 8 },
   { x: 19, y: 8 },
+  { x: 20, y: 8 },
   { x: 21, y: 8 },
+  { x: 22, y: 8 },
   { x: 23, y: 8 },
   { x: 52, y: 8 },
+  { x: 53, y: 8 },
   { x: 54, y: 8 },
+  { x: 55, y: 8 },
   { x: 56, y: 8 },
+  { x: 57, y: 8 },
   { x: 58, y: 8 },
+  { x: 59, y: 8 },
   { x: 60, y: 8 },
   { x: 15, y: 9 },
+  { x: 16, y: 9 },
   { x: 17, y: 9 },
+  { x: 18, y: 9 },
   { x: 19, y: 9 },
+  { x: 20, y: 9 },
   { x: 21, y: 9 },
+  { x: 22, y: 9 },
   { x: 23, y: 9 },
+  { x: 24, y: 9 },
   { x: 25, y: 9 },
+  { x: 26, y: 9 },
   { x: 49, y: 9 },
+  { x: 50, y: 9 },
   { x: 51, y: 9 },
+  { x: 52, y: 9 },
   { x: 53, y: 9 },
+  { x: 54, y: 9 },
   { x: 55, y: 9 },
+  { x: 56, y: 9 },
   { x: 57, y: 9 },
+  { x: 58, y: 9 },
   { x: 59, y: 9 },
+  { x: 60, y: 9 },
   { x: 61, y: 9 },
   { x: 14, y: 10 },
+  { x: 15, y: 10 },
   { x: 16, y: 10 },
+  { x: 17, y: 10 },
   { x: 18, y: 10 },
+  { x: 19, y: 10 },
   { x: 20, y: 10 },
+  { x: 21, y: 10 },
   { x: 22, y: 10 },
+  { x: 23, y: 10 },
   { x: 24, y: 10 },
+  { x: 25, y: 10 },
   { x: 26, y: 10 },
+  { x: 27, y: 10 },
   { x: 28, y: 10 },
   { x: 47, y: 10 },
+  { x: 48, y: 10 },
   { x: 49, y: 10 },
+  { x: 50, y: 10 },
   { x: 51, y: 10 },
+  { x: 52, y: 10 },
   { x: 53, y: 10 },
+  { x: 54, y: 10 },
   { x: 55, y: 10 },
+  { x: 56, y: 10 },
   { x: 57, y: 10 },
+  { x: 58, y: 10 },
   { x: 59, y: 10 },
+  { x: 60, y: 10 },
   { x: 61, y: 10 },
   { x: 14, y: 11 },
   { x: 21, y: 11 },
+  { x: 22, y: 11 },
   { x: 23, y: 11 },
+  { x: 24, y: 11 },
   { x: 25, y: 11 },
+  { x: 26, y: 11 },
   { x: 27, y: 11 },
+  { x: 28, y: 11 },
   { x: 29, y: 11 },
+  { x: 30, y: 11 },
   { x: 46, y: 11 },
+  { x: 47, y: 11 },
   { x: 48, y: 11 },
+  { x: 49, y: 11 },
   { x: 50, y: 11 },
+  { x: 51, y: 11 },
   { x: 52, y: 11 },
+  { x: 53, y: 11 },
   { x: 54, y: 11 },
   { x: 61, y: 11 },
+  { x: 62, y: 11 },
   { x: 24, y: 12 },
   { x: 25, y: 12 },
+  { x: 26, y: 12 },
   { x: 27, y: 12 },
+  { x: 28, y: 12 },
   { x: 29, y: 12 },
+  { x: 30, y: 12 },
   { x: 31, y: 12 },
   { x: 44, y: 12 },
+  { x: 45, y: 12 },
   { x: 46, y: 12 },
+  { x: 47, y: 12 },
   { x: 48, y: 12 },
+  { x: 49, y: 12 },
+  { x: 50, y: 12 },
   { x: 51, y: 12 },
   { x: 26, y: 13 },
+  { x: 27, y: 13 },
   { x: 28, y: 13 },
+  { x: 29, y: 13 },
   { x: 30, y: 13 },
+  { x: 31, y: 13 },
   { x: 32, y: 13 },
   { x: 43, y: 13 },
+  { x: 44, y: 13 },
   { x: 45, y: 13 },
+  { x: 46, y: 13 },
   { x: 47, y: 13 },
+  { x: 48, y: 13 },
   { x: 49, y: 13 },
   { x: 27, y: 14 },
+  { x: 28, y: 14 },
   { x: 29, y: 14 },
+  { x: 30, y: 14 },
   { x: 31, y: 14 },
+  { x: 32, y: 14 },
   { x: 43, y: 14 },
+  { x: 44, y: 14 },
   { x: 45, y: 14 },
+  { x: 46, y: 14 },
+  { x: 47, y: 14 },
   { x: 48, y: 14 },
   { x: 29, y: 15 },
+  { x: 30, y: 15 },
   { x: 45, y: 15 },
+  { x: 46, y: 15 },
   { x: 47, y: 15 },
   { x: 30, y: 16 },
   { x: 31, y: 16 },
   { x: 44, y: 16 },
+  { x: 45, y: 16 },
   { x: 46, y: 16 },
   { x: 31, y: 17 },
   { x: 44, y: 17 },
   { x: 45, y: 17 },
   { x: 21, y: 18 },
+  { x: 22, y: 18 },
   { x: 23, y: 18 },
   { x: 32, y: 18 },
   { x: 44, y: 18 },
   { x: 53, y: 18 },
   { x: 54, y: 18 },
   { x: 19, y: 19 },
+  { x: 27, y: 19 },
   { x: 28, y: 19 },
   { x: 32, y: 19 },
   { x: 43, y: 19 },
   { x: 48, y: 19 },
   { x: 56, y: 19 },
-  { x: 57, y: 19 },
   { x: 18, y: 20 },
   { x: 22, y: 20 },
   { x: 23, y: 20 },
+  { x: 24, y: 20 },
   { x: 25, y: 20 },
   { x: 46, y: 20 },
   { x: 50, y: 20 },
+  { x: 51, y: 20 },
   { x: 52, y: 20 },
   { x: 53, y: 20 },
   { x: 58, y: 20 },
   { x: 17, y: 21 },
+  { x: 19, y: 21 },
   { x: 20, y: 21 },
+  { x: 21, y: 21 },
   { x: 22, y: 21 },
+  { x: 23, y: 21 },
   { x: 24, y: 21 },
+  { x: 25, y: 21 },
   { x: 26, y: 21 },
+  { x: 27, y: 21 },
   { x: 28, y: 21 },
   { x: 45, y: 21 },
+  { x: 47, y: 21 },
   { x: 48, y: 21 },
+  { x: 49, y: 21 },
   { x: 50, y: 21 },
+  { x: 51, y: 21 },
   { x: 52, y: 21 },
+  { x: 53, y: 21 },
   { x: 54, y: 21 },
+  { x: 55, y: 21 },
   { x: 56, y: 21 },
   { x: 16, y: 22 },
   { x: 18, y: 22 },
@@ -158,6 +252,7 @@ const maskPoints = [
   { x: 30, y: 22 },
   { x: 31, y: 22 },
   { x: 44, y: 22 },
+  { x: 45, y: 22 },
   { x: 46, y: 22 },
   { x: 47, y: 22 },
   { x: 48, y: 22 },
@@ -257,33 +352,22 @@ const maskPoints = [
   { x: 42, y: 36 },
   { x: 49, y: 36 },
   { x: 50, y: 36 },
-  { x: 12, y: 37 },
   { x: 23, y: 37 },
   { x: 24, y: 37 },
   { x: 33, y: 37 },
   { x: 42, y: 37 },
   { x: 51, y: 37 },
   { x: 52, y: 37 },
-  { x: 12, y: 38 },
-  { x: 13, y: 38 },
   { x: 20, y: 38 },
   { x: 21, y: 38 },
   { x: 42, y: 38 },
   { x: 54, y: 38 },
   { x: 55, y: 38 },
-  { x: 62, y: 38 },
-  { x: 63, y: 38 },
-  { x: 13, y: 39 },
-  { x: 16, y: 39 },
   { x: 17, y: 39 },
   { x: 34, y: 39 },
   { x: 41, y: 39 },
   { x: 58, y: 39 },
   { x: 59, y: 39 },
-  { x: 62, y: 39 },
-  { x: 63, y: 39 },
-  { x: 13, y: 40 },
-  { x: 14, y: 40 },
   { x: 17, y: 40 },
   { x: 18, y: 40 },
   { x: 32, y: 40 },
@@ -296,17 +380,11 @@ const maskPoints = [
   { x: 57, y: 40 },
   { x: 58, y: 40 },
   { x: 59, y: 40 },
-  { x: 62, y: 40 },
-  { x: 14, y: 41 },
   { x: 17, y: 41 },
   { x: 18, y: 41 },
   { x: 19, y: 41 },
   { x: 57, y: 41 },
   { x: 58, y: 41 },
-  { x: 61, y: 41 },
-  { x: 62, y: 41 },
-  { x: 14, y: 42 },
-  { x: 15, y: 42 },
   { x: 18, y: 42 },
   { x: 19, y: 42 },
   { x: 34, y: 42 },
@@ -318,9 +396,6 @@ const maskPoints = [
   { x: 42, y: 42 },
   { x: 56, y: 42 },
   { x: 57, y: 42 },
-  { x: 61, y: 42 },
-  { x: 14, y: 43 },
-  { x: 15, y: 43 },
   { x: 19, y: 43 },
   { x: 20, y: 43 },
   { x: 33, y: 43 },
@@ -336,10 +411,6 @@ const maskPoints = [
   { x: 55, y: 43 },
   { x: 56, y: 43 },
   { x: 57, y: 43 },
-  { x: 60, y: 43 },
-  { x: 61, y: 43 },
-  { x: 15, y: 44 },
-  { x: 16, y: 44 },
   { x: 19, y: 44 },
   { x: 20, y: 44 },
   { x: 21, y: 44 },
@@ -356,9 +427,6 @@ const maskPoints = [
   { x: 54, y: 44 },
   { x: 55, y: 44 },
   { x: 56, y: 44 },
-  { x: 60, y: 44 },
-  { x: 15, y: 45 },
-  { x: 16, y: 45 },
   { x: 20, y: 45 },
   { x: 21, y: 45 },
   { x: 22, y: 45 },
@@ -377,10 +445,6 @@ const maskPoints = [
   { x: 53, y: 45 },
   { x: 54, y: 45 },
   { x: 55, y: 45 },
-  { x: 59, y: 45 },
-  { x: 60, y: 45 },
-  { x: 16, y: 46 },
-  { x: 17, y: 46 },
   { x: 21, y: 46 },
   { x: 22, y: 46 },
   { x: 23, y: 46 },
@@ -410,9 +474,6 @@ const maskPoints = [
   { x: 53, y: 46 },
   { x: 54, y: 46 },
   { x: 55, y: 46 },
-  { x: 59, y: 46 },
-  { x: 17, y: 47 },
-  { x: 18, y: 47 },
   { x: 22, y: 47 },
   { x: 23, y: 47 },
   { x: 24, y: 47 },
@@ -439,13 +500,6 @@ const maskPoints = [
   { x: 52, y: 47 },
   { x: 53, y: 47 },
   { x: 54, y: 47 },
-  { x: 58, y: 47 },
-  { x: 59, y: 47 },
-  { x: 17, y: 48 },
-  { x: 18, y: 48 },
-  { x: 57, y: 48 },
-  { x: 58, y: 48 },
-  { x: 18, y: 49 },
   { x: 34, y: 49 },
   { x: 35, y: 49 },
   { x: 36, y: 49 },
@@ -454,24 +508,13 @@ const maskPoints = [
   { x: 39, y: 49 },
   { x: 40, y: 49 },
   { x: 41, y: 49 },
-  { x: 57, y: 49 },
-  { x: 19, y: 50 },
-  { x: 57, y: 50 },
-  { x: 19, y: 51 },
-  { x: 56, y: 51 },
-  { x: 20, y: 52 },
-  { x: 55, y: 52 },
-  { x: 21, y: 53 },
-  { x: 54, y: 54 },
-  { x: 22, y: 55 },
+  { x: 43, y: 49 },
   { x: 35, y: 55 },
   { x: 36, y: 55 },
   { x: 37, y: 55 },
   { x: 38, y: 55 },
   { x: 39, y: 55 },
   { x: 40, y: 55 },
-  { x: 53, y: 55 },
-  { x: 23, y: 56 },
   { x: 36, y: 56 },
   { x: 37, y: 56 },
   { x: 38, y: 56 },
@@ -494,12 +537,18 @@ const maskPoints = [
   { x: 37, y: 61 },
   { x: 38, y: 61 },
   { x: 39, y: 61 },
+  { x: 36, y: 62 },
+  { x: 37, y: 62 },
+  { x: 38, y: 62 },
   { x: 39, y: 62 },
   { x: 36, y: 63 },
   { x: 37, y: 63 },
   { x: 38, y: 63 },
   { x: 39, y: 63 },
   { x: 36, y: 64 },
+  { x: 37, y: 64 },
+  { x: 38, y: 64 },
+  { x: 39, y: 64 },
   { x: 36, y: 65 },
   { x: 37, y: 65 },
   { x: 38, y: 65 },
@@ -513,6 +562,8 @@ const maskPoints = [
   { x: 38, y: 67 },
   { x: 39, y: 67 },
   { x: 37, y: 68 },
+  { x: 38, y: 68 },
+  { x: 39, y: 68 },
   { x: 37, y: 69 },
   { x: 38, y: 69 },
   { x: 39, y: 69 },
@@ -522,11 +573,27 @@ const maskPoints = [
 ];
 
 class Point {
-  constructor(x, y, vx, vy) {
+  constructor(x, y, vx, vy, live, immortal) {
     this.x = x;
     this.y = y;
     this.vx = vx;
     this.vy = vy;
+    this.live = live;
+    this.immortal = immortal;
+    this.ticksLived = 0;
+    this.midLiveLevel = Math.floor(live / 2);
+  }
+
+  get liveAlpha() {
+    if (this.immortal) {
+      return 1.0;
+    } else {
+      if (this.ticksLived < this.midLiveLevel) {
+        return Math.min(0.3, this.ticksLived / this.midLiveLevel);
+      } else {
+        return Math.min(0.1, (this.live - this.ticksLived) / this.midLiveLevel);
+      }
+    }
   }
 
   static randomPointInRange(width, height) {
@@ -537,7 +604,9 @@ class Point {
       randomIntegerInRange(0, width),
       randomIntegerInRange(0, height),
       randomIntegerInRange(-30, 30) / 25,
-      randomIntegerInRange(-30, 30) / 25
+      randomIntegerInRange(-30, 30) / 25,
+      randomIntegerInRange(minLifeTicks, maxLifeTicks),
+      false
     );
   }
 }
@@ -552,7 +621,7 @@ class Line {
 
 class Scene {
   #pointsCount = 500;
-  #maxLineLength = 4000;
+  #maxLineLength = 2000;
 
   constructor(width, height, maskPoints) {
     this.width = width;
@@ -566,10 +635,15 @@ class Scene {
     }
 
     maskPoints.forEach((p) => {
-      console.log("P ", p.y);
-
       this.points.push(
-        new Point(width / 2 - 400 + p.x * 10, 50 + p.y * 10, 0, 0)
+        new Point(
+          width / 2 - 400 + p.x * 10,
+          50 + p.y * 10,
+          0,
+          0,
+          undefined,
+          true
+        )
       );
     });
   }
@@ -587,27 +661,38 @@ class Scene {
   // Private methods
 
   #movePoints = function () {
+    let alivePoints = [];
     this.points.forEach((point) => {
-      let nextX = point.x + point.vx;
-      let nextY = point.y + point.vy;
+      if (point.immortal == true || point.ticksLived < point.live) {
+        let nextX = point.x + point.vx;
+        let nextY = point.y + point.vy;
 
-      // adjust X to be in bounds box
-      if (nextX < 0) {
-        nextX = this.width + nextX;
-      } else if (nextX > this.width) {
-        nextX = nextX - this.width;
+        // adjust X to be in bounds box
+        if (nextX < 0) {
+          nextX = this.width + nextX;
+        } else if (nextX > this.width) {
+          nextX = nextX - this.width;
+        }
+
+        // adjust Y to be in bounds box
+        if (nextY < 0) {
+          nextY = this.height + nextY;
+        } else if (nextY > this.height) {
+          nextY = nextY - this.height;
+        }
+
+        point.x = nextX;
+        point.y = nextY;
+        point.ticksLived += 1;
+        alivePoints.push(point);
       }
-
-      // adjust Y to be in bounds box
-      if (nextY < 0) {
-        nextY = this.height + nextY;
-      } else if (nextY > this.height) {
-        nextY = nextY - this.height;
-      }
-
-      point.x = nextX;
-      point.y = nextY;
     });
+    const pointsDied = this.points.length - alivePoints.length;
+    this.points = alivePoints;
+
+    for (let index = 0; index < pointsDied; index++) {
+      this.points.push(Point.randomPointInRange(this.width, this.height));
+    }
   };
 
   #connectPointsWithLines = function (points, maxLineLength) {
@@ -676,6 +761,10 @@ function drawLoop(ctx, scene) {
   drawPoints(ctx, scene.points);
   drawLines(ctx, scene.lines);
 
+  ctx.fillStyle = "white";
+  ctx.fillText(`Points: ${scene.points.length}`, 10, 10);
+  ctx.fillText(`Lines: ${scene.lines.length}`, 10, 25);
+
   setTimeout(() => drawLoop(ctx, scene), 20);
 }
 
@@ -694,8 +783,10 @@ function drawPoints(ctx, points) {
   for (let index = 0; index < points.length; index++) {
     const point = points[index];
 
-    ctx.fillStyle = "white";
-    ctx.fillRect(point.x, point.y, 2, 2);
+    let alpha = point.liveAlpha;
+    let size = 2;
+    ctx.fillStyle = `rgb(255 255 255 / ${Math.round(alpha * 100)}%)`;
+    ctx.fillRect(point.x, point.y, size, size);
   }
 }
 
@@ -705,9 +796,30 @@ function drawLines(ctx, lines) {
     path.moveTo(line.p1.x, line.p1.y);
     path.lineTo(line.p2.x, line.p2.y);
 
-    const alpha = Math.floor(line.weight * 100);
+    if (line.p1.immortal && line.p2.immortal) {
+      ctx.strokeStyle = "white";
+    } else {
+      // Create gradients
+      const linGrad = ctx.createLinearGradient(
+        line.p1.x,
+        line.p1.y,
+        line.p2.x,
+        line.p2.y
+      );
 
-    ctx.strokeStyle = `rgb(200 200 200 / ${alpha}%)`;
+      const p1Color = line.p1.immortal
+        ? "rgb(255 255 255 / 0%)"
+        : `rgb(255 255 255 / ${line.p1.liveAlpha * 100}%)`;
+      const p2Color = line.p2.immortal
+        ? "rgb(255 255 255 / 0%)"
+        : `rgb(255 255 255 / ${line.p2.liveAlpha * 100}%)`;
+
+      linGrad.addColorStop(0, p1Color);
+      linGrad.addColorStop(1, p2Color);
+
+      ctx.strokeStyle = linGrad;
+    }
+
     ctx.lineWidth = line.weight > 0.8 ? 2 : 1;
     ctx.stroke(path);
   });
